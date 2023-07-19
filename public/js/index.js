d50630524bb8549b8f26c1a12035f8dd66ecc22f
Maria Fredriksson <mf223wk@student.lnu.se>
2023-07-19 09:22:05 +0200

    Works with socket io now

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,18 @@
 import '../socket.io/socket.io.js'
 
-// Create a socket connection using Socket.IO.
-const socket = window.io()
+const issueList = document.querySelector('#issue-list')
 
-// Listen for "snippet/close" message from the server.
-socket.on('snippet/close', (issueIid) => changeIssueState('closed', issueIid))
+// Only create a socket connection if there is an issue list on the page.
+if (issueList) {
+  // Create a socket connection using Socket.IO.
+  const socket = window.io()
 
-// Listen for "snippet/open" message from the server.
-socket.on('snippet/open', (issueIid) => changeIssueState('opened', issueIid))
+  // Listen for "snippet/close" message from the server.
+  socket.on('snippet/close', (issueIid) => changeIssueState('closed', issueIid))
+
+  // Listen for "snippet/open" message from the server.
+  socket.on('snippet/open', (issueIid) => changeIssueState('opened', issueIid))
+}
 
 /**
  * Change the issue state in the table.
@@ -17,8 +22,6 @@ socket.on('snippet/open', (issueIid) => changeIssueState('opened', issueIid))
  */
 function changeIssueState (newState, issueIid) {
   console.log('Hej!')
-
-  const issueList = document.querySelector('#issue-list')
 
   console.log(issueList)
   console.log(issueIid)
@@ -32,5 +35,26 @@ function changeIssueState (newState, issueIid) {
     // Update the issue state in the table
     const stateCell = issueRow.querySelector('.state-column')
     stateCell.textContent = newState
+
+    const form = issueRow.querySelector('form')
+    const button = issueRow.querySelector('button')
+
+    if (newState === 'closed') {
+      // Change the form action
+      form.action = `/snippets/${issueIid}/open`
+
+      // Change the button text and class
+      button.textContent = 'Open issue'
+      button.classList.remove('btn-primary', 'btn-opened')
+      button.classList.add('btn-secondary', 'btn-closed')
+    } else {
+      // Change the form action
+      form.action = `/snippets/${issueIid}/close`
+
+      // Change the button text and class
+      button.textContent = 'Close issue'
+      button.classList.remove('btn-secondary', 'btn-closed')
+      button.classList.add('btn-primary', 'btn-opened')
+    }
   }
 }
