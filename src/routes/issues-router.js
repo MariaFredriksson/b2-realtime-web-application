cb66158f0ebaf6d9fc1b9314a21ff9b897a380bf
Maria Fredriksson <mf223wk@student.lnu.se>
2023-07-19 10:49:08 +0200

    Delete some old code in issues controller and router

--- src/routes/issues-router.js
+++ src/routes/issues-router.js
@@ -7,12 +7,10 @@
 
 import express from 'express'
 import { IssuesController } from '../controllers/issues-controller.js'
-import { AuthorizationController } from '../controllers/authorization-controller.js'
 
 export const router = express.Router()
 
 const controller = new IssuesController()
-const authorizationController = new AuthorizationController()
 
 // Map HTTP verbs and route paths to controller action methods.
 
@@ -21,12 +19,3 @@ router.get('/', (req, res, next) => controller.index(req, res, next))
 router.post('/:iid/close', controller.closePost)
 
 router.post('/:iid/open', controller.openPost)
-
-router.get('/create', authorizationController.userLoggedIn, controller.create)
-router.post('/create', authorizationController.userLoggedIn, controller.createPost)
-
-router.get('/:id/update', authorizationController.userLoggedIn, authorizationController.userIsAuthor, controller.update)
-router.post('/:id/update', authorizationController.userLoggedIn, authorizationController.userIsAuthor, controller.updatePost)
-
-router.get('/:id/delete', authorizationController.userLoggedIn, authorizationController.userIsAuthor, controller.delete)
-router.post('/:id/delete', authorizationController.userLoggedIn, authorizationController.userIsAuthor, controller.deletePost)
