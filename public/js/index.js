0fb6e85ec4b4e705473c10f59bcbdd72ab6ec23c
Maria Fredriksson <mf223wk@student.lnu.se>
2023-07-20 10:47:53 +0200

    Add so it is detected when an issue is created or updated, and the page updates

--- public/js/index.js
+++ public/js/index.js
@@ -12,6 +12,12 @@ if (issueList) {
 
   // Listen for "issues/open" message from the server.
   socket.on('issues/open', (issueIid) => changeIssueState('opened', issueIid))
+
+  // Listen for "issues/create" message from the server.
+  socket.on('issues/create', (issue) => createIssue(issue))
+
+  // Listen for "issues/update" message from the server.
+  socket.on('issues/update', (issue) => updateIssue(issue))
 }
 
 /**
@@ -56,5 +62,66 @@ function changeIssueState (newState, issueIid) {
       button.classList.remove('btn-secondary', 'btn-closed')
       button.classList.add('btn-primary', 'btn-opened')
     }
+  }
+}
+
+/**
+ * Create a new issue in the table.
+ *
+ * @param {object} issue - The issue object.
+ */
+function createIssue (issue) {
+  // Only add the issue if it already doesn't exist
+  if (!issueList.querySelector(`[data-id="${issue.iid}"]`)) {
+    console.log(issue)
+
+    const issueNode = document.querySelector('#issue-template').content.cloneNode(true)
+
+    issueNode.querySelector('tr').setAttribute('data-id', issue.iid)
+
+    updateNodeInfo(issueNode, issue)
+
+    issueList.appendChild(issueNode)
+  }
+}
+
+/**
+ * Update an existing issue in the table.
+ *
+ * @param {object} issue - The issue object.
+ */
+function updateIssue (issue) {
+  const issueNode = issueList.querySelector(`[data-id="${issue.iid}"]`)
+
+  if (issueNode) {
+    console.log(issue)
+
+    // Update all the issue information, just because there isn't so much of it right now.
+    updateNodeInfo(issueNode, issue)
+  }
+}
+
+/**
+ * Update the issue node with the issue information.
+ *
+ * @param {object} node - The issue node.
+ * @param {object} issue - The issue object.
+ */
+function updateNodeInfo (node, issue) {
+  const [imgCell, titleCell, descriptionCell, stateCell, stateBtnCell] = node.querySelectorAll('td')
+
+  imgCell.querySelector('img').src = issue.ownerAvatar
+  titleCell.textContent = issue.title
+  descriptionCell.textContent = issue.description
+  stateCell.textContent = issue.state
+
+  if (issue.state === 'opened') {
+    stateBtnCell.querySelector('form').action = `/issues/${issue.iid}/close`
+    stateBtnCell.querySelector('button').textContent = 'Close issue'
+    stateBtnCell.querySelector('button').classList.add('btn-primary', 'btn-opened')
+  } else {
+    stateBtnCell.querySelector('form').action = `/issues/${issue.iid}/open`
+    stateBtnCell.querySelector('button').textContent = 'Open issue'
+    stateBtnCell.querySelector('button').classList.add('btn-secondary', 'btn-closed')
   }
 }
--- src/controllers/webhooks-controller.js
+++ src/controllers/webhooks-controller.js
@@ -51,10 +51,25 @@ export class WebhooksController {
 
       // If we have received something with an iid, we have most likely received an issue, and then we can emit an event.
       if (iid) {
-        if (req.body.object_attributes.state === 'opened') {
+        // TODO: Add so that it can be detected in issue is created or if it is updated, and emit suitable events for that.
+
+        // If the issue has action open, that means that it has been created
+        if (req.body.object_attributes.action === 'open') {
+          const issueInfo = this.#getIssueInfo(req.body)
+          res.io.emit('issues/create', issueInfo)
+
+          // If the action is reopen, that means that the issue was closed but has been reopened
+        } else if (req.body.object_attributes.action === 'reopen') {
           res.io.emit('issues/open', iid)
-        } else if (req.body.object_attributes.state === 'closed') {
+
+          // If the action is close, that means that the issue was closed
+        } else if (req.body.object_attributes.action === 'close') {
           res.io.emit('issues/close', iid)
+
+          // If the action is update, that means that the issue was updated
+        } else if (req.body.object_attributes.action === 'update') {
+          const issueInfo = this.#getIssueInfo(req.body)
+          res.io.emit('issues/update', issueInfo)
         }
       }
     } catch (error) {
@@ -62,5 +77,23 @@ export class WebhooksController {
       err.status = 500
       next(err)
     }
+  }
+
+  /**
+   * Gets some specific information about the issue from a request body.
+   *
+   * @param {object} requestBody - The request body.
+   * @returns {object} - The issue information.
+   */
+  #getIssueInfo (requestBody) {
+    const issueInfo = {
+      title: requestBody.object_attributes.title,
+      description: requestBody.object_attributes.description,
+      id: requestBody.object_attributes.id,
+      iid: requestBody.object_attributes.iid,
+      state: requestBody.object_attributes.state,
+      ownerAvatar: requestBody.user.avatar_url
+    }
+    return issueInfo
   }
 }
